0eee7d0d94e7d533cec5b8de8454e9d17ece6963
Halvor <Halvor.b.westgaard@gmail.com>
2024-12-04 19:57:04 +0100

    First commit

---
+++ WebAppTest/Default.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+
+
+    <h1> Overskrift  </h1>
+
+    <nav>
+
+        <a href="Loginn.aspx">Log inn</a>
+
+    </nav>
+
+    <heder></heder>
+
+
+    <style>
+        .gray-box {
+            background-color: gray;
+            width: 100%;
+        }
+
+
+
+        body {
+            background-color: lightgrey;
+        }
+
+        h1 {
+            font-size: 40px;
+            background-color: dimgray;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2
+        }
+
+        th {
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="LabelMan" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Label ID="LabelTir" runat="server" Text=""></asp:Label>
+    </form>
+    <p>
+        &nbsp;
+    </p>
+    <p>
+        &nbsp;
+    </p>
+ 
+</body>
+</html>
---
+++ WebAppTest/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Meny", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            foreach (DataRow row in dt.Rows)
+            {
+
+                LabelMan.Text = row[1].ToString();
+
+            }
+                
+
+        }
+
+        protected void ButtonSearchLastName_Click(object sender, EventArgs e)
+        {
+            //SqlParameter param;
+            //var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            //DataTable dt = new DataTable();
+            //using (SqlConnection conn = new SqlConnection(connectionString))
+            //{
+            //    conn.Open();
+
+            //    SqlCommand cmd = new SqlCommand("SELECT * from Person where etternavn = @lastName", conn);
+            //    cmd.CommandType = CommandType.Text;
+
+            //    //params here
+            //    param = new SqlParameter("@lastName", SqlDbType.NChar);
+            //    param.Value = TextBoxLastName.Text; //variabel som blir sendt inn til metoden. Kommer fra bruker som tastet dette inn i et tekstfelt.
+            //    cmd.Parameters.Add(param);
+
+                
+
+            //    SqlDataReader reader = cmd.ExecuteReader();
+            //    dt.Load(reader);
+            //    reader.Close();
+            //    conn.Close();
+            //}
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+        }
+
+        protected void ButtonSearchFirstName_Click(object sender, EventArgs e)
+        {
+            //SqlParameter param;
+            //var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            //DataTable dt = new DataTable();
+            //using (SqlConnection conn = new SqlConnection(connectionString))
+            //{
+            //    conn.Open();
+
+            //    SqlCommand cmd = new SqlCommand("SELECT * from Person where fornavn = @forNavn", conn);
+            //    cmd.CommandType = CommandType.Text;
+
+            //    //params here
+            //    param = new SqlParameter("@forNavn", SqlDbType.NChar);
+            //    param.Value = TextBoxFirstName.Text; //variabel som blir sendt inn til metoden. Kommer fra bruker som tastet dette inn i et tekstfelt.
+            //    cmd.Parameters.Add(param);
+
+
+
+            //    SqlDataReader reader = cmd.ExecuteReader();
+            //    dt.Load(reader);
+            //    reader.Close();
+            //    conn.Close();
+            //}
+            //GridView1.DataSource = dt;
+            //GridView1.DataBind();
+        }
+    }
+}
